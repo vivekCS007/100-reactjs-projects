4efa835f3555cd8dc4e9ba489bb1042b0762e869
Rishika Purbey <rishikapurbey712@gmail.com>
2026-04-27 13:38:14 +0530

    feat: add ShowTime cinematic streaming guide React project

--- config/project-Item.tsx
+++ config/project-Item.tsx
@@ -10,4 +10,12 @@ export const projectItemConfig: ProjectItems[] = [
     liveLink: "https://sendlyfile.vercel.app/",
     techStack: ["Next.js", "TypeScript", "Tailwind", "Supabase", "Prisma"],
   },
+  {
+    projectName: "ShowTime",
+    description: "A cinematic streaming guide built with React featuring real-time movie search and a personalised watchlist to track your favourites.",
+    projectImage: "showtime.png",
+    githubLink: "https://github.com/Rishikapurbey/ShowTime",
+    liveLink: "https://harmonious-maamoul-e4854c.netlify.app/",
+    techStack: ["React js", "JavaScript", "CSS","HTML"],
+},
 ];
